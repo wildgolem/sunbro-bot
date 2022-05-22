0a2a4256e8436c63a31b9495132c9d679e689a43
wildgolem <wildgolem@hotmail.com>
2022-05-22 15:30:31 -0400

    init

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -4,20 +4,7 @@ module.exports = {
     on: true,
     execute(oldMember, newMember) {
         if (!newMember.user.bot) {
-            if (newMember.roles.cache.has(casul) && newMember.roles.cache.has(voice)) {
-                newMember.voice.disconnect();
-                newMember.send({
-                    embeds: [{
-                        color: 15844367,
-                        author: {
-                            name: `${newMember.user.username},`,
-                            icon_url: `${newMember.user.avatarURL()}`,
-                        },
-                        description: `You need a class to join a voice chat.\nGo to <#949940867769188422> at once!`,
-                        image: { url: 'https://cdn.discordapp.com/attachments/771978143699042334/977739605019349033/gitgud.png' }
-                    }]
-                }).catch(err => { });
-            } else if (newMember.roles.cache.has(explorer) && newMember.roles.cache.has(voice)) {
+            if (newMember.roles.cache.has(explorer) && newMember.roles.cache.has(voice)) {
                 newMember.voice.setMute(false).catch(err => { });
                 newMember.voice.setDeaf(false).catch(err => { });
 
