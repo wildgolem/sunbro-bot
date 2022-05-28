d243b152915fe297b6dc94c978b2442b0a15bd5f
wildgolem <wildgolem@hotmail.com>
2022-05-27 22:08:19 -0400

    init

--- events/ready.js
+++ events/ready.js
@@ -16,11 +16,11 @@ module.exports = {
 				🟢${guild.members.cache.filter(m => m.presence?.status == 'online').size}  
 				🌙${guild.members.cache.filter(m => m.presence?.status == 'idle').size}  
 				⛔${guild.members.cache.filter(m => m.presence?.status == 'dnd').size}`);
-			guild.channels.cache.get('979867714099220490').setName(`Bowman: ${guild.roles.cache.get(bowman).members.size}`);
-			guild.channels.cache.get('979867725671317564').setName(`Magician: ${guild.roles.cache.get(magician).members.size}`);
-			guild.channels.cache.get('979867737201455124').setName(`Thief: ${guild.roles.cache.get(thief).members.size}`);
-			guild.channels.cache.get('979867748450582628').setName(`Warrior: ${guild.roles.cache.get(warrior).members.size}`);
-			guild.channels.cache.get('979867757996810311').setName(`Pirate: ${guild.roles.cache.get(pirate).members.size}`);
+			guild.channels.cache.get('979867714099220490').setName(`𝙱𝚘𝚠𝚖𝚊𝚗: ${guild.roles.cache.get(bowman).members.size}`);
+			guild.channels.cache.get('979867725671317564').setName(`𝙼𝚊𝚐𝚒𝚌𝚒𝚊𝚗: ${guild.roles.cache.get(magician).members.size}`);
+			guild.channels.cache.get('979867737201455124').setName(`𝚃𝚑𝚒𝚎𝚏: ${guild.roles.cache.get(thief).members.size}`);
+			guild.channels.cache.get('979867748450582628').setName(`𝚆𝚊𝚛𝚛𝚒𝚘𝚛: ${guild.roles.cache.get(warrior).members.size}`);
+			guild.channels.cache.get('979867757996810311').setName(`𝙿𝚒𝚛𝚊𝚝𝚎: ${guild.roles.cache.get(pirate).members.size}`);
 		} statusCount();
 
 		setInterval(() => {
