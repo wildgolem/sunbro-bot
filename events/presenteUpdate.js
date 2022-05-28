3e2f66bf593b5602d40c320861bf9b3ee2f08231
wildgolem <wildgolem@hotmail.com>
2022-05-28 01:53:37 -0400

    init

--- events/presenteUpdate.js
+++ events/presenteUpdate.js
@@ -8,11 +8,5 @@ module.exports = {
         } else {
             newPresence.member.roles.remove(explorer);
         };
-
-        newPresence.member.guild.channels.cache.get('979911051787268176').setName(`
-            👥${newPresence.member.guild.members.cache.filter(m => !m.user.bot).size}  
-            🟢${newPresence.member.guild.members.cache.filter(m => m.presence?.status == 'online').size}  
-			🌙${newPresence.member.guild.members.cache.filter(m => m.presence?.status == 'idle').size}  
-			⛔${newPresence.member.guild.members.cache.filter(m => m.presence?.status == 'dnd').size}`);
     }
 };
