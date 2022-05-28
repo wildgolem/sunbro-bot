dcee485c2ef9cef1fb31b8f4677dd13da7d984e1
wildgolem <wildgolem@hotmail.com>
2022-05-27 20:49:19 -0400

    init

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -4,20 +4,7 @@ module.exports = {
     on: true,
     execute(oldMember, newMember) {
         if (!newMember.user.bot) {
-            if (newMember.roles.cache.has(explorer) && newMember.roles.cache.has(voice)) {
-                newMember.voice.setMute(false).catch(err => { });
-                newMember.voice.setDeaf(false).catch(err => { });
-
-                /* newMember.guild.channels.cache.get(freemarket).send({
-                    embeds: [{
-                        color: 15844367,
-                        author: {
-                            name: `${newMember.user.username} has joined a voice chat!`,
-                            icon_url: `${newMember.user.avatarURL()}`,
-                        }
-                    }]
-                }).then(msg => { setTimeout(() => msg.delete().catch(err => { }), 30000) }).catch(err => { }); */
-            } else if (!newMember.roles.cache.has(explorer) && newMember.roles.cache.has(voice)) {
+            if (!newMember.roles.cache.has(explorer) && newMember.roles.cache.has(voice)) {
                 newMember.voice.disconnect();
                 newMember.send({
                     embeds: [{
