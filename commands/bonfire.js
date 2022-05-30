82d7abe73e82d350de9856f4bcb7ec92e064dbac
wildgolem <wildgolem@hotmail.com>
2022-05-30 15:59:43 -0400

    init

--- commands/bonfire.js
+++ commands/bonfire.js
@@ -37,7 +37,7 @@ module.exports = {
       .setColor("#f1c40f")
       .setTitle("Howdy, casul.")
       .setFooter({text: "Select your class to git gud:"})
-      .setImage('https://cdn.discordapp.com/attachments/980907403279228958/980914101029072926/bonfire.png');
+      .setImage('https://cdn.discordapp.com/attachments/980907403279228958/980923358159994940/bonfire.png');
 		await interaction.channel.send({ embeds: [embedMessage], components: [row] });
 	},
 };
